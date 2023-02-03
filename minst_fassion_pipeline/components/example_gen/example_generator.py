a407cf536318e093157e8b10f7add89528bce874
astrackomni <69056244+astrackomni@users.noreply.github.com>
2023-02-03 15:02:04 +0000

    Add minst data loader

---
+++ minst_fassion_pipeline/components/example_gen/example_generator.py
@@ -0,0 +1,56 @@
+from tfx.components.example_gen.import_example_gen.component import ImportExampleGen
+import tensorflow as tf
+import os
+import numpy as np
+from absl import logging
+from tqdm import tqdm
+from pathlib import Path
+import tempfile
+
+
+def _bytes_feature(value):
+    """Returns a bytes_list from a string / byte."""
+    if isinstance(value, type(tf.constant(0))):
+        value = value.numpy()
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _int64_feature(value):
+    """Returns an int64_list from a bool / enum / int / uint."""
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _serialize_example(image, label):
+    feature = {
+        'image': _bytes_feature(tf.io.serialize_tensor(image)),
+        'label': _int64_feature(label)
+    }
+    example_proto = tf.train.Example(
+        features=tf.train.Features(feature=feature))
+    return example_proto.SerializeToString()
+
+
+def _write_tfrecord(images, labels, file: Path):
+    with tf.io.TFRecordWriter(str(file)) as writer:
+        for image, label in tqdm(zip(images, labels)):
+            serialized_example = _serialize_example(image, label)
+            writer.write(serialized_example)
+
+
+def _generate_tfrecord(output_dir: Path = Path(os.getcwd()) / "temp_data"):
+    logging.info('Generating raw data')
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    data = np.concatenate([x_train, x_test], axis=0)
+    labels = np.concatenate([y_train, y_test], axis=0)
+    logging.info('Writing raw data to tfrecord')
+    _write_tfrecord(data, labels, output_dir / 'fashion_mnist.tfrecord')
+
+
+def get_example_gen() -> ImportExampleGen:
+    temp_root = Path(tempfile.mkdtemp(prefix='tfx-data'))  # Create a temporary directory.
+    _generate_tfrecord(temp_root)
+
+    # Create an instance of the ExampleGen component
+    logging.info('Creating ExampleGen component from tfrecord')
+    example_gen = ImportExampleGen(input_base=str(temp_root))
+    return example_gen
---
+++ minst_fassion_pipeline/local_runner.py
@@ -0,0 +1,49 @@
+import os
+from typing import Optional, List, Text
+from absl import logging
+from ml_metadata.proto import metadata_store_pb2
+import tfx.v1 as tfx
+from components.example_gen.example_generator import get_example_gen
+
+PIPELINE_NAME = 'minst_pipeline'
+PIPELINE_ROOT = os.path.join('.', PIPELINE_NAME + '_root')
+METADATA_PATH = os.path.join('.', 'tfx_metadata', PIPELINE_NAME, 'metadata.db')
+ENABLE_CACHE = True
+
+
+def create_pipeline(
+    pipeline_name: Text,
+    pipeline_root: Text,
+    enable_cache: Optional[bool] = False,
+    metadata_connection_config: Optional[metadata_store_pb2.ConnectionConfig] = None,
+    beam_pipeline_args: Optional[List[Text]] = None,
+) -> tfx.dsl.Pipeline:
+    components = []
+
+    example_gen = get_example_gen()
+    components.append(example_gen)
+
+    return tfx.dsl.Pipeline(
+        pipeline_name=pipeline_name,
+        pipeline_root=pipeline_root,
+        components=components,
+        enable_cache=enable_cache,
+        metadata_connection_config=metadata_connection_config,
+        beam_pipeline_args=beam_pipeline_args,
+    )
+
+
+def run_pipeline():
+    my_pipeline = create_pipeline(
+        pipeline_name=PIPELINE_NAME,
+        pipeline_root=PIPELINE_ROOT,
+        enable_cache=ENABLE_CACHE,
+        metadata_connection_config=tfx.orchestration.metadata.sqlite_metadata_connection_config(
+            METADATA_PATH)
+    )
+    tfx.orchestration.LocalDagRunner().run(my_pipeline)
+
+
+if __name__ == '__main__':
+    logging.set_verbosity(logging.INFO)
+    run_pipeline()
